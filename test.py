e4fac359a0840c82834a5471a07f677b88d8a184
taro <taro@dlas.jp>
2021-01-05 17:41:38 +0900

    test

---
+++ test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+import os
+import re
+import csv
+import sys
+import copy
+import glob
+import time
+import datetime
+import codecs
+import traceback
+
+from operator import itemgetter
+
+
+if __name__ == '__main__':
+
+    try:
+
+        print 'a'
+
+    except:
+        print traceback.format_exc()
+raw_input( '--- end ---' )
